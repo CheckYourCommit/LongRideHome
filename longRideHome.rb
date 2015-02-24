1575d7a1a89de36b552a1d24bbb7cad474458c00
CheckYourCommit <TorySue1339@aol.com>
2015-02-24 14:56:03 -0600

    Adding File
    Adding the initial file for LongRideHome

---
+++ longRideHome.rb
@@ -0,0 +1,316 @@
+
+length = 3000
+wallet = 4000
+
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+
+puts "                         Welcome to Long Ride Home!"
+puts ""
+puts ""
+puts "         You woke up in a strange town on the other side of the country"
+puts "           and need to get home. You check your wallet. You have $#{wallet}"
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+puts ""
+sleep(5)
+
+
+nextCity = rand(5..25).to_i
+cityAfter = nextCity * 2
+inTown = true
+spaceController = " "
+
+while length > 0
+
+	while inTown 
+
+		trainTicket = (nextCity*2) * 3
+	    busTicket = nextCity * 2
+
+
+	    puts ""
+	    puts ""
+	    puts ""
+	    puts ""
+	    puts ""
+	    puts "                                                         wallet: $#{wallet}"
+	    puts ""
+	    puts ""
+	    puts "    _________              _________               _________           "
+		puts "   /        /________     /        /_________     /        /_________  "
+		puts "  /________/        /|   /________/         /|   /________/         /| "
+		puts "  |        |_______/ |   |        |________/ |   |        |________/ | "
+		puts "  |        |       | /___|        |        | /___|        |        | | "
+		puts "  |        |       ||   ||        |        ||   ||        |        | | "
+		puts "  |        |       ||   ||        |        ||   ||        |        | | "
+		puts "  |        |       ||   ||        |        ||   ||        |        |   "
+		puts "  -----------------------Welcome to Town------------------------------ "
+		puts ""
+		puts "You are #{length} miles from home"
+		puts ""
+		puts "What would you like to do?"
+		puts "        Walk                             (walk)"
+		puts "        Check a Train Station             (train)"
+		puts "        Check the Bus Station            (bus)"
+
+		action = gets.chomp
+
+		if action.casecmp("walk") == 0
+			time = nextCity / 2
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                           __                                      " 
+			puts "                           ||___________                           "
+			puts "                           |  ________  \\                         "
+			puts "                           |____________/                          "
+			puts "                 __________||                                      "
+			puts "                /   _____  ||                                      "
+			puts "                \\__________||___________                          "
+			puts "                           |    _______ \\                         "
+			puts "                           |____________/                          "
+			puts "                           ||                                      "
+			puts "                           ||                                      "
+			puts "                           ||                                      "
+			puts "                           ||                                      "
+			puts "                           ||                                      "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "You can get to the next city (#{nextCity} miles) in #{time} seconds"
+			puts "Walk (walk)          Go back to town (town)"
+			action = gets.chomp
+
+			if action.casecmp("walk") == 0
+					puts "You start your walk!"
+					spaceEcho = (74.0 / nextCity)
+					puts "#{spaceEcho}"
+					while nextCity > 0
+						
+						if nextCity % 2 == 0
+							actionTop = spaceController +     " o"
+							actionMiddle = spaceController +  "-|-"
+							actionBottom = spaceController +  " ^"
+                        else
+							actionTop = spaceController +     " o"
+							actionMiddle = spaceController +  " |-"
+							actionBottom = spaceController +  " ^"
+						end
+
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+		                puts "#{actionTop}"
+						puts "#{actionMiddle}"
+						puts "#{actionBottom}"
+
+						nextCity = nextCity.pred
+						length = length.pred
+						sleep(1.0 / 3.0)
+
+						spaceController = spaceController + (" " * spaceEcho)
+						inTown = false
+					end
+					spaceController = " "
+
+			elsif action.casecmp("town") == 0
+				puts "You have gone back to town"
+				sleep(1.0)
+			end
+		elsif action.casecmp("train") == 0
+
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                         _________________                         "
+			puts "                         | Train Station |                         "
+			puts "               |=========|===============|=====+==|                "
+			puts "               |   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=  |                "
+			puts "            ___|__________________________________|____            "
+			puts "       _____|_________________________________________|____        "
+			puts "       |==================================================|        "
+			puts "       |                                                  |        "
+			puts "       |  []  []  []  []  []  []  []  []   []   []   []   |        "
+			puts "       |                                                  |        "
+			puts "       |                     ______                       |        "
+			puts "       |                     |    |                       |        " 
+			puts "        +++++++++++++++++++++|    |++++++++++++++++++++++++        "
+			puts "                                                                   "
+			puts "You can get two cities over (#{cityAfter} miles) for $#{trainTicket}"
+			puts "Buy Ticket (Buy)          Leave the station (Leave)"
+			action = gets.chomp
+
+			if action.casecmp("buy") == 0
+				if wallet > trainTicket
+					puts "You have bought a ticket! All Aboard!"
+					spaceEcho = (66.0 / cityAfter)
+					while cityAfter > 0
+						actionTop = spaceController +     " |DD|____T_"
+						actionMiddle = spaceController +  " |_ |_____|<"
+						actionBottom = spaceController +  "    @-@-@-oo\\"
+
+
+
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+		                puts "#{actionTop}"
+						puts "#{actionMiddle}"
+						puts "#{actionBottom}"
+
+						cityAfter = cityAfter.pred
+						length = length.pred
+						sleep(1.0 / 10.0)
+
+						spaceController = spaceController + (" " * spaceEcho)
+						inTown = false
+					end
+					spaceController = " "
+				else 
+					puts "You don't have money for a ticket. :("
+				end
+			elsif action.casecmp("leave") == 0
+				puts "You have left the train station!"
+			end
+		elsif action.casecmp("bus") == 0
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "   
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                                                                   "
+			puts "                         _____________                             "
+			puts "                         |   Buses   |                             "
+			puts "       __________________|___________|_____________________        "
+			puts "       |                                                  |        "
+			puts "       |  o o o o         o o o o        o o o o     o o  |        "
+			puts "       |                                                  |        "
+			puts "       | o o         o o     ______   o o        o o      |        "
+			puts "       |                     |    |                       |        " 
+			puts "                                                                   "			
+			puts ""
+			puts "You can get to the next city (#{cityAfter} miles) for $#{busTicket}"
+			puts "Buy Ticket (Buy)          Leave the terminal (Leave)"
+			action = gets.chomp
+
+			if action.casecmp("buy") == 0
+				if wallet > busTicket
+					puts "You have bought a ticket! Go on in!"
+					spaceEcho = (74.0 / nextCity)
+					while nextCity > 0
+						
+						actionTop = spaceController +     "[o>][o]\\"
+						actionMiddle = spaceController +  "L_______&"
+						actionBottom = spaceController +  " ()   ()"
+
+
+
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+						puts ""
+		                puts "#{actionTop}"
+						puts "#{actionMiddle}"
+						puts "#{actionBottom}"
+
+						nextCity = nextCity.pred
+						length = length.pred
+						sleep(1.0 / 5.0)
+
+						spaceController = spaceController + (" " * spaceEcho)
+						inTown = false
+					end
+					spaceController = " "
+				else 
+					puts "You don't have money for a ticket. :("
+				end
+			elsif action.casecmp("leave") == 0
+				puts "You have left the Bus Te
+				rminal!"
+			end
+		end
+	end
+
+	nextCity = rand(5..25).to_i
+	cityAfter = nextCity * 2
+	inTown = true
+
+end
